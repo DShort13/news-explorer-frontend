8099e154b4ba584795008e24193b60b66e370f81
DShort13 <dion.short@outlook.com>
2025-02-13 12:20:16 +0000

    Add basic structure of Header component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "../../assets/react.svg";
 import viteLogo from "/vite.svg";
+import Header from "../Header/Header";
 import "./App.css";
 
 function App() {
@@ -8,8 +9,7 @@ function App() {
 
   return (
     <>
-      <h1>Hello Vite!</h1>
-      <p>This is your first Vite project</p>
+      <Header />
     </>
   );
 }
