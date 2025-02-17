040ca7c7c479e6f48e333a6aebe93f67d59177ad
DShort13 <dion.short@outlook.com>
2025-02-17 09:16:25 +0100

    Finish basic structure of Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,13 @@
+import "./Header.css";
+
 function Header() {
   return (
     <header className="header">
-      <h1>Hello!</h1>
+      <h1 className="header__logo">NewsExplorer</h1>
+      <div className="header__user-container">
+        <p className="header__home">Home</p>
+        <button className="header__signin">Sign in</button>
+      </div>
     </header>
   );
 }
