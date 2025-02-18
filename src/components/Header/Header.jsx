0843447bc903548bdba051277641614a301d58dc
DShort13 <dion.short@outlook.com>
2025-02-19 00:03:46 +0100

    Create Navigation component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,18 @@
+import Navigation from "../Navigation/Navigation";
 import "./Header.css";
 
 function Header() {
   return (
-    <header className="header">
-      <h1 className="header__logo">NewsExplorer</h1>
-      <div className="header__user-container">
-        <p className="header__home">Home</p>
-        <button className="header__signin">Sign in</button>
+    <header className="header__container">
+      <div className="header">
+        <p className="header__logo">NewsExplorer</p>
+        <div className="header__user-container">
+          <p className="header__home">Home</p>
+          <button className="header__signin">Sign in</button>
+        </div>
+      </div>
+      <div className="navigation__container">
+        <Navigation />
       </div>
     </header>
   );
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+import "./Navigation.css";
+
+function Navigation() {
+  return (
+    <div className="navigation">
+      <h1 className="main">What's going on in the world?</h1>
+      <p>
+        Find the latest news on any topic and save them in your personal
+        account.
+      </p>
+    </div>
+  );
+}
+
+export default Navigation;
