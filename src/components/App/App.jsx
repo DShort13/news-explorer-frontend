e0e5662d4174104e5197d22a90847d4efab0d4fe
DShort13 <dion.short@outlook.com>
2025-02-23 14:43:35 +0000

    style: remove default body margin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
+import "./App.css";
 import Header from "../Header/Header";
 import About from "../About/About";
 import Footer from "../Footer/Footer";
-import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
-    <>
-      <Header />
-      <About />
-      <Footer />
-    </>
+    <div className="page">
+      <div className="page__content">
+        <Header />
+        <About />
+        <Footer />
+      </div>
+    </div>
   );
 }
 
