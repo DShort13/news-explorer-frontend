a75a89d48c7f28d3f7de52881b1de2c62f36f1fc
DShort13 <dion.short@outlook.com>
2025-02-27 14:52:32 +0000

    Create NewsCardList component and move About component to the Main component

--- src/components/Main/Main/Main.jsx
+++ src/components/Main/Main/Main.jsx
@@ -1,21 +1,10 @@
+import NewsCardList from "../../NewsCardList/NewsCardList";
 import About from "../../About/About";
-import { defaultNewsArticles } from "../../../utils/defaultNewsArticles";
 
 function Main() {
   return (
     <main>
-      <section className="articles">
-        <ul className="articles__list">
-          {defaultNewsArticles.map((item) => {
-            return (
-              <div key={item._id}>
-                <p>{item.source.name}</p>
-                <img className="articles__item" src={item.urlToImage} alt="" />
-              </div>
-            );
-          })}
-        </ul>
-      </section>
+      <NewsCardList />
       <About />
     </main>
   );
--- src/components/NewsCardList/NewsCardList.jsx
+++ src/components/NewsCardList/NewsCardList.jsx
@@ -1,5 +1,20 @@
+import { defaultNewsArticles } from "../../utils/defaultNewsArticles";
+
 function NewsCardList() {
-  return <div></div>;
+  return (
+    <div>
+      <ul className="articles__list">
+        {defaultNewsArticles.map((item) => {
+          return (
+            <div key={item._id}>
+              <p>{item.source.name}</p>
+              <img className="articles__item" src={item.urlToImage} alt="" />
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default NewsCardList;
