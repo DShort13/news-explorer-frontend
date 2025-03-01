588793c4595246d452eb4d57bab5881a81e74399
DShort13 <dion.short@outlook.com>
2025-03-01 13:08:58 +0000

    style: add css to SavedText and SavedNews

--- src/components/SavedNews/SavedNews.jsx
+++ src/components/SavedNews/SavedNews.jsx
@@ -1,10 +1,17 @@
 import Navigation from "../Navigation/Navigation";
+import SavedText from "../SavedText/SavedText";
+import "./SavedNews.css";
 
 function SavedNews() {
   return (
     <div>
       <Navigation />
-      <h2>SavedNews</h2>
+      <section className="saved__info">
+        <p>Saved articles</p>
+        <p>Dion, you have 5 saved articles</p>
+        <p>By keywords</p>
+      </section>
+      <SavedText />
     </div>
   );
 }
--- src/components/SavedText/SavedText.jsx
+++ src/components/SavedText/SavedText.jsx
@@ -1,3 +1,11 @@
-function SavedText() {}
+import "./SavedText.css";
+
+function SavedText() {
+  return (
+    <div className="saved-text__container">
+      <h2 className="saved-text__title">SavedNews</h2>
+    </div>
+  );
+}
 
 export default SavedText;
